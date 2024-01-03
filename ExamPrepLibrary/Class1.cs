6a49b5152b50172d91a2531ca73959431ddf2cf5
Nick Ward <nickward@microsoft.com>
2024-01-03 13:42:38 +1100

    added class library

---
+++ ExamPrepLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace ExamPrepLibrary;
+
+public class Exam
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+
+    public Exam(string name, string code)
+    {
+        Name = name;
+        Code = code;
+    }
+
+    
+    public bool PassExam()
+    {
+        return true;
+    }
+}
